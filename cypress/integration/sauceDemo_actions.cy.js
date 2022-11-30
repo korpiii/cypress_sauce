bda6d7e01124f02ee6621d64beefc775d80dd379
korpiii <da.dnvl@live.com>
2022-11-29 19:14:45 -0600

    Sets method to extract data from fixture file

--- cypress/integration/sauceDemo_actions.cy.js
+++ cypress/integration/sauceDemo_actions.cy.js
@@ -1,7 +1,16 @@
 /// <reference types="Cypress" />
 
 describe('Full shopping flow', () => {
-  it('Should visit store homepage', () => {
+
+	 //Pulls data from fixture file before testing starts
+	before(function () {
+		cy.fixture('example').then(function (data) {
+		this.data = data;
+		})
+  	})
+
+	it('Should visit store homepage', () => {
     cy.visit('https://www.saucedemo.com/');
   })
+
 })
