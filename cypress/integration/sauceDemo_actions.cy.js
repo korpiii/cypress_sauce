efcb5b6356d8eab56c2a307c79e9906834ac6c6f
korpiii <da.dnvl@live.com>
2022-12-05 20:03:45 -0600

    'Adds items to cart' test case

--- cypress/integration/sauceDemo_actions.cy.js
+++ cypress/integration/sauceDemo_actions.cy.js
@@ -11,14 +11,19 @@ describe('Full shopping flow', () => {
 
 	it('Logs in', function () {
 		cy.visit('https://www.saucedemo.com/');
- 		cy.get('#user-name').type(this.fakeData.username);
-		cy.get('#password').type(this.fakeData.password);
-		cy.get('#login-button').click();
+ 		cy.get('#user-name').type(this.fakeData.username)
+		cy.get('#password').type(this.fakeData.password)
+		cy.get('#login-button').click()
   	})
 
 	it('Verifies there are items to buy', () => {
 		cy.get('.inventory_item').should('have.length.at.least', 1)
     })
 
+	it('Adds items to cart', () => {
+		cy.get('#add-to-cart-sauce-labs-backpack').click()
+		cy.get('#add-to-cart-sauce-labs-bike-light').click()
+
+    })
 
 })
